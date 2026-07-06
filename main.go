c7f60f94909bedd9b5e16d98f531d1ad307d4923
eulerbutcooler <eulerbutcooler@gmail.com>
2026-07-06 17:02:18 +0530

    Used channels instead of mutexes. No race conditions now.

--- main.go
+++ main.go
@@ -4,49 +4,95 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
 
 // TODO:
-// HTTP Server
-// Upgrade to websocket once client connects
-// Add newly connected WS to server
-// Add WS client
-// Remove client on disconnect
-// Send broadcast message with no race conditions
+// [x]HTTP Server
+// [x]Upgrade to websocket once client connects
+// [x]Add newly connected WS to server
+// [x]Add WS client
+// []Remove client on disconnect
+// []Send broadcast message with no race conditions
+
+type MsgType string
 
 const (
-	WSPort = ":4444"
+	WSPort                   = ":4444"
+	MsgTypeBroadcast MsgType = "broadcast"
 )
 
+type ReqMsg struct {
+	MsgType MsgType
+	Client  *Client
+	Data    string
+}
+
 type Client struct {
-	ID   uuid.UUID
-	mu   *sync.RWMutex
+	ID uuid.UUID
+	// mu   *sync.RWMutex
 	conn *websocket.Conn
 }
 
 type Server struct {
-	clients []*Client
-	mu      *sync.RWMutex
+	clients map[uuid.UUID]*Client
+	// mu      *sync.RWMutex
+	joinCh  chan *Client
+	leaveCh chan *Client
 }
 
 func NewClient(conn *websocket.Conn) *Client {
 	ID := uuid.New()
 	return &Client{
-		ID:   ID,
-		mu:   new(sync.RWMutex),
+		ID: ID,
+		// mu:   new(sync.RWMutex),
 		conn: conn,
 	}
 }
 
 func NewServer() *Server {
 	return &Server{
-		clients: []*Client{},
-		mu:      new(sync.RWMutex),
+		clients: map[uuid.UUID]*Client{},
+		// mu:      new(sync.RWMutex),
+		joinCh:  make(chan *Client, 64),
+		leaveCh: make(chan *Client, 64),
 	}
+}
+
+func (c *Client) readMsgLoop(leaveServerCh chan<- *Client) {
+	defer func() {
+		c.conn.Close()
+		leaveServerCh <- c
+	}()
+	_, b, err := c.conn.ReadMessage()
+	if err != nil {
+		return
+	}
+}
+
+func (s *Server) AcceptLoop() {
+	for {
+		select {
+		case c := <-s.joinCh:
+			// join logic
+			s.joinServer(c)
+		case c := <-s.leaveCh:
+			// leave logic
+			s.leaveServer(c)
+		}
+	}
+}
+
+func (s *Server) joinServer(c *Client) {
+	s.clients[c.ID] = c
+	fmt.Printf("client joined the server, cID: %s\n", c.ID)
+}
+
+func (s *Server) leaveServer(c *Client) {
+	delete(s.clients, c.ID)
+	fmt.Printf("client left the server, cID: %s\n", c.ID)
 }
 
 func (s *Server) handleWS(w http.ResponseWriter, r *http.Request) {
@@ -63,17 +109,19 @@ func (s *Server) handleWS(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	client := NewClient(conn)
+	s.joinCh <- client
 	// Bad solution.
 	// NOTE: Dont communicate by sharing memory. Share memory by communicating.
-	s.mu.Lock()
-	s.clients = append(s.clients, client)
-	fmt.Println(len(s.clients))
-	s.mu.Unlock()
+	// s.mu.Lock()
+	// s.clients = append(s.clients, client)
+	// fmt.Println(len(s.clients))
+	// s.mu.Unlock()
 
 }
 
 func createWSServer() {
 	s := NewServer()
+	go s.AcceptLoop()
 	http.HandleFunc("/", s.handleWS)
 	log.Printf("Websocket server listening on %s", WSPort)
 	log.Fatal(http.ListenAndServe(WSPort, nil))
